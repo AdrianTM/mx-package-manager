b2e29cd01cfcc7f77a3c05b6e5ecd9971942de82
Adrian <adrian@mxlinux.org>
2017-04-06 16:20:14 -0400

    added dependency, new build

--- mx-package-manager_0.17.4.dsc
+++ mx-package-manager_0.17.4.dsc
@@ -13,20 +13,20 @@ Build-Depends: qt5-qmake, debhelper (>= 7.0.50~)
 Package-List:
  mx-package-manager deb admin optional arch=any
 Checksums-Sha1:
- 9346392a08d96ef6a93ee5b13d6a50efd8d02c3e 102080 mx-package-manager_0.17.4.tar.xz
+ 21b9a3fc7d9848742dff3d2b78a17707274633fa 71576 mx-package-manager_0.17.4.tar.xz
 Checksums-Sha256:
- cea5b0deb9dc03163d4ef14c4f8c981a2047b342aaf8fddca05de575fa0ab653 102080 mx-package-manager_0.17.4.tar.xz
+ cc07aad2052d5650206babb3b1dd68613d50d9cef400761c9cdeea7fbd033800 71576 mx-package-manager_0.17.4.tar.xz
 Files:
- 915c5684285418700e8db8c6bd89c1f0 102080 mx-package-manager_0.17.4.tar.xz
+ 097089426eeacb4668c49a1b330483ed 71576 mx-package-manager_0.17.4.tar.xz
 
 -----BEGIN PGP SIGNATURE-----
 Version: GnuPG v1
 
-iQEcBAEBAgAGBQJY4wV1AAoJEHCTjHgGee6Y0mQH/RWE431wo5AOeu+ICMKsD5Rj
-79opOLbhP2v7RLH0yjXqPqvQ3w1mcWVoZRzOMq1RJL5jn9GFO0Suu3YC1VjK0O84
-177SN+aL5eJ9SxbbOtfX89ZCm1Sx5uuvFM3iZxIFsFQyhZfovgpnfDD7kTmfC5qR
-OCBKrdQI3+ePV3cu3JBxGIxO0P5H5fx/+x6g5WsDdraxHA56fabSs3EhFMPmNYOf
-2i1c3ajDI//JpWt1JANU1xaCY2Iy/gBW6CoKiT9V1csgADMAwdmlDgHUZntwANei
-CAHwhrEFt8FUKLo217RCVUifs/V0W5GSM+FMCFtfh2OP+b2y1Ma78E1BaBIuklM=
-=46qx
+iQEcBAEBAgAGBQJY5qLHAAoJEHCTjHgGee6Y9t0IALcLzLcbDDAdrawZJJhZNPhv
+1dX3FO8Ud3V+Jb1aAdMAGEhw749tOVuSdxOecqygi0SduvsywRoZ4K2dz5aOEEiB
+cR2OTs427WufZITYCyUATNxPSjc/m9pGjX+K5JBCjhHUzpL8poqDh9zugThlSOGy
+CHh76XdqTehzIHeF8j5pJ5sTjNLgmyXh+0YOlg/jY2cgP+IKtUjEh0nIMcZnkkiK
+l5qLfWGxA+ZhHl4M4UZtBYmEBZSBGQdxEVR/JNCrSp2KdATJm4OLnB/6rXFDpf3t
+sHDYPO8akUm8WOMaCi4qvJtakprqxXjAPQ/P7ePqU4zz9vpKkFsXEj8Uh7Ul69M=
+=Q4qZ
 -----END PGP SIGNATURE-----
--- translations/mx-package-manager_ja.ts
+++ translations/mx-package-manager_ja.ts
@@ -4,161 +4,204 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.ui" line="14"/>
-        <location filename="../mainwindow.cpp" line="55"/>
-        <location filename="../mainwindow.cpp" line="328"/>
-        <location filename="../mainwindow.cpp" line="335"/>
-        <location filename="../mainwindow.cpp" line="344"/>
+        <location filename="../mainwindow.ui" line="20"/>
+        <location filename="../mainwindow.cpp" line="59"/>
+        <location filename="../mainwindow.cpp" line="434"/>
+        <location filename="../mainwindow.cpp" line="441"/>
+        <location filename="../mainwindow.cpp" line="450"/>
         <source>MX Package Manager</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="31"/>
+        <location filename="../mainwindow.ui" line="42"/>
         <source>Popular applications</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="72"/>
-        <location filename="../mainwindow.cpp" line="61"/>
+        <location filename="../mainwindow.ui" line="83"/>
+        <location filename="../mainwindow.cpp" line="65"/>
         <source>Package</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="77"/>
-        <location filename="../mainwindow.cpp" line="61"/>
-        <location filename="../mainwindow.cpp" line="274"/>
+        <location filename="../mainwindow.ui" line="88"/>
+        <location filename="../mainwindow.cpp" line="65"/>
+        <location filename="../mainwindow.cpp" line="409"/>
         <source>Info</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="82"/>
-        <location filename="../mainwindow.cpp" line="61"/>
+        <location filename="../mainwindow.ui" line="93"/>
+        <location filename="../mainwindow.cpp" line="65"/>
         <source>Description</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="90"/>
+        <location filename="../mainwindow.ui" line="101"/>
         <source>Use option --yes for apt-get install</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="100"/>
+        <location filename="../mainwindow.ui" line="111"/>
         <source>search</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="110"/>
+        <location filename="../mainwindow.ui" line="121"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:16pt;&quot;&gt;Install additional packages&lt;/span&gt;&lt;/p&gt;&lt;p&gt;To use this feature, make sure you are connected to the internet.&lt;/p&gt;&lt;p&gt;Greyed out items have already been installed.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="131"/>
+        <location filename="../mainwindow.ui" line="148"/>
         <source>MX Test Repo</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="136"/>
+        <location filename="../mainwindow.ui" line="159"/>
         <source>Debian Backports Repo</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="201"/>
+        <location filename="../mainwindow.ui" line="328"/>
         <source>Display help </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="204"/>
+        <location filename="../mainwindow.ui" line="331"/>
         <source>Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="212"/>
+        <location filename="../mainwindow.ui" line="339"/>
         <source>Alt+H</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="263"/>
+        <location filename="../mainwindow.ui" line="352"/>
+        <source>Uninstall</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="193"/>
         <source>About this application</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="266"/>
+        <location filename="../mainwindow.ui" line="196"/>
         <source>About...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="274"/>
+        <location filename="../mainwindow.ui" line="204"/>
         <source>Alt+B</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="306"/>
+        <location filename="../mainwindow.ui" line="269"/>
         <source>Quit application</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="309"/>
+        <location filename="../mainwindow.ui" line="272"/>
         <source>Close</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="317"/>
+        <location filename="../mainwindow.ui" line="280"/>
         <source>Alt+N</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="333"/>
+        <location filename="../mainwindow.ui" line="242"/>
+        <location filename="../mainwindow.cpp" line="482"/>
         <source>Install</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="327"/>
+        <location filename="../mainwindow.cpp" line="433"/>
         <source>About MX Package Manager</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="328"/>
+        <location filename="../mainwindow.cpp" line="434"/>
         <source>Version: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="329"/>
+        <location filename="../mainwindow.cpp" line="435"/>
         <source>Package Manager for MX Linux</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="331"/>
+        <location filename="../mainwindow.cpp" line="437"/>
         <source>Copyright (c) MX Linux</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="274"/>
-        <location filename="../mainwindow.cpp" line="332"/>
+        <location filename="../mainwindow.cpp" line="409"/>
+        <location filename="../mainwindow.cpp" line="438"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="273"/>
+        <location filename="../mainwindow.cpp" line="90"/>
+        <source>Running apt-get update... </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="222"/>
+        <source>Please wait...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="306"/>
+        <source>Pre-processing for </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="311"/>
+        <source>Installing </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="315"/>
+        <source>Post-processing for </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="335"/>
+        <source>Done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="336"/>
+        <source>Process finished.&lt;p&gt;&lt;b&gt;Do you want to exit MX Package Installer?&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="337"/>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="337"/>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="408"/>
         <source>Packages to be installed: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="333"/>
-        <location filename="../mainwindow.cpp" line="335"/>
+        <location filename="../mainwindow.cpp" line="439"/>
+        <location filename="../mainwindow.cpp" line="441"/>
         <source>License</source>
         <translation type="unfinished"></translation>
     </message>
-</context>
-<context>
-    <name>QApplication</name>
     <message>
-        <location filename="../main.cpp" line="51"/>
-        <source>Unable to get exclusive lock</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../main.cpp" line="52"/>
-        <source>Another package management application (like Synaptic or apt-get), is already running. Please close that application first</source>
+        <location filename="../mainwindow.cpp" line="480"/>
+        <source>Reinstall</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
